3d76cc892517b44084e2b0dbb737df5d3a673948
dmarcial <dmarcialgraciano@apple.com>
2025-06-24 09:51:24 -0600

    Changed Names and setters

--- src/main/java/com/example/demo/data/user.java
+++ src/main/java/com/example/demo/data/user.java
@@ -1,8 +1,12 @@
 package com.example.demo.data;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Setter
+@Getter
 @Document(collection = "users")
 public class user {
     @Id
@@ -10,27 +14,4 @@ public class user {
     private String name;
     private String phone;
 
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 }
